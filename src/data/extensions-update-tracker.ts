16ba70f9a70f764ba157e9ba438743bdde0fb03c
Maxim Topciu <me@maximtop.dev>
2026-09-15 19:02:10 +0300

    Restore Extensions Update Tracker pages (#6)
    * Restore Extensions Update Tracker pages
    
    * Add Update Tracker product visuals

---
+++ src/data/extensions-update-tracker.ts
@@ -0,0 +1,25 @@
+import { extensionStoreLinks } from './store-links';
+
+export const extensionsUpdateTracker = {
+  name: 'Extensions Update Tracker',
+  baseUrl: '/extensions/update-tracker/',
+  pageUrl: 'https://maximtop.dev/extensions/update-tracker/',
+  chromeStoreUrl: extensionStoreLinks.extensionsUpdateTracker.chrome.url,
+  edgeStoreUrl: extensionStoreLinks.extensionsUpdateTracker.edge.url,
+  firefoxStoreUrl: extensionStoreLinks.extensionsUpdateTracker.firefox.url,
+  githubUrl: 'https://github.com/maximtop/extensions-update-tracker',
+  issuesUrl: 'https://github.com/maximtop/extensions-update-tracker/issues',
+  iconUrl: '/extensions/update-tracker/icon.png',
+  ogImageUrl: '/extensions/update-tracker/og.png',
+  contactEmail: 'me@maximtop.dev',
+} as const;
+
+export const extensionsUpdateTrackerNav = [
+  { name: 'Overview', href: extensionsUpdateTracker.baseUrl, key: 'overview' },
+  { name: 'How it works', href: `${extensionsUpdateTracker.baseUrl}how-it-works/`, key: 'how-it-works' },
+  { name: 'FAQ', href: `${extensionsUpdateTracker.baseUrl}faq/`, key: 'faq' },
+  { name: 'Privacy', href: `${extensionsUpdateTracker.baseUrl}privacy/`, key: 'privacy' },
+  { name: 'Support', href: `${extensionsUpdateTracker.baseUrl}support/`, key: 'support' },
+] as const;
+
+export type ExtensionsUpdateTrackerPage = (typeof extensionsUpdateTrackerNav)[number]['key'];
